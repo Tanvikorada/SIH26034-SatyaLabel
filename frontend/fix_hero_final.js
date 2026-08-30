1ff060c3295774832eee4a9877a868e5bcf61074
Korada Tanvi <tanvikorada@gmail.com>
2026-08-30 22:39:55 +0530

    fix(build): remove ESLint-breaking scripts to unblock Vercel deployments

--- frontend/fix_hero_final.js
+++
@@ -1,8 +0,0 @@
-const fs = require('fs');
-let code = fs.readFileSync('app/page.jsx', 'utf8');
-
-code = code.replace(/emblem-cutout\.jpg/g, 'emblem-transparent.png');
-code = code.replace(/ mix-blend-multiply dark:invert dark:mix-blend-screen/g, '');
-
-fs.writeFileSync('app/page.jsx', code);
-console.log("HERO PNG FIXED");
